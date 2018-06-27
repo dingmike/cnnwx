2199b866d6d8cf4ec9b851d9e5f7dd35b5c42c3a
slateding <mikezhangding@gmail.com>
2018-06-27 11:24:46 +0800

    gongDu

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,52 +5,57 @@ const user = require('../../services/user.js');
 //获取应用实例
 const app = getApp()
 Page({
-  data: {
-    newGoods: [],
-    hotGoods: [],
-    topics: [],
-    brands: [],
-    floorGoods: [],
-    banner: [],
-    channel: []
-  },
-  onShareAppMessage: function () {
-    return {
-      title: 'NideShop',
-      desc: 'FecsTec English',
-      path: '/pages/index/index'
-    }
-  },
+    data: {
+        newGoods: [],
+        hotGoods: [],
+        topics: [],
+        brands: [],
+        floorGoods: [],
+        banner: [],
+        channel: []
+    },
 
-  getIndexData: function () {
-    let that = this;
-    util.request(api.IndexUrl).then(function (res) {
-      if (res.errno === 0) {
-        that.setData({
-          newGoods: res.data.newGoodsList,
-          hotGoods: res.data.hotGoodsList,
-          topics: res.data.topicList,
-          brand: res.data.brandList,
-          floorGoods: res.data.categoryList,
-          banner: res.data.banner,
-          channel: res.data.channel
+    showToast() {
+        let $toast = this.selectComponent(".J_toast")
+        $toast && $toast.show()
+    },
+    onShareAppMessage: function () {
+        return {
+            title: 'NideShop',
+            desc: 'FecsTec English',
+            path: '/pages/index/index'
+        }
+    },
+
+/*    getIndexData: function () {
+        let that = this;
+        util.request(api.IndexUrl).then(function (res) {
+            if (res.errno === 0) {
+                that.setData({
+                    newGoods: res.data.newGoodsList,
+                    hotGoods: res.data.hotGoodsList,
+                    topics: res.data.topicList,
+                    brand: res.data.brandList,
+                    floorGoods: res.data.categoryList,
+                    banner: res.data.banner,
+                    channel: res.data.channel
+                });
+            }
         });
-      }
-    });
-  },
-  onLoad: function (options) {
-    this.getIndexData();
-  },
-  onReady: function () {
-    // 页面渲染完成
-  },
-  onShow: function () {
-    // 页面显示
-  },
-  onHide: function () {
-    // 页面隐藏
-  },
-  onUnload: function () {
-    // 页面关闭
-  },
+    },*/
+    onLoad: function (options) {
+        // this.getIndexData();
+    },
+    onReady: function () {
+        // 页面渲染完成
+    },
+    onShow: function () {
+        // 页面显示
+    },
+    onHide: function () {
+        // 页面隐藏
+    },
+    onUnload: function () {
+        // 页面关闭
+    },
 })
