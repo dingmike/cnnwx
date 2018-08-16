608defd33a4f73d96c39f256eab1a0ea74ed48d1
slateding <mikezhangding@gmail.com>
2018-08-16 18:04:25 +0800

    add gongdu authdfddddddd

--- pages/ucenter/order/order.js
+++ pages/ucenter/order/order.js
@@ -14,93 +14,72 @@ Page({
         refresh: false,
         orderStatus: '',
         realTotalPages: 1,
-        listHeight:100
+        listHeight: 0,
+        pageStatus: 1,
+        listStatus: 1,
     },
     onLoad: function (options) {
         // 页面初始化 options为页面跳转所带来的参数
         // 页面显示
-        wx.showLoading({
-            title: '加载中...',
-            success: function () {
-
-            }
-        });
-
         this.getOrderList();
     },
-    /*onPullDownRefresh(){
-        debugger
+   // 出错重新加载按钮事件
+    reloadHandler() {
         this.setData({
-            page: 1,
-            orderList: [],
-            size: 5
+            pageStatus: 1
         });
-        // 增加下拉刷新数据的功能
-        this.refresh = true;
-        this.getOrderList();
-    },*/
+        this.getOrderList(true);
+    },
     /**
      * 页面上拉触底事件的处理函数
      */
-    onReachBottom () {
+   /* onReachBottom () {
         console.log("下一页");
         this.getOrderList();
-    },
+    },*/
     scrollToLowerHandler() {
-        debugger
-        if(this.data.nomore){
-            this.getOrderList();
-        }
+        3 != this.data.listStatus &&  this.getOrderList(true);
     },
-    getOrderList(){
+    getOrderList(flag){
         let that = this;
-        if (that.data.totalPages <= that.data.page - 1&&!that.data.refresh) {
-            that.setData({
-                nomore: true
-            })
-            return;
-        }
+        that.setData({
+            listStatus: 2
+        })
         util.request(api.OrderList, {page: that.data.page, size: that.data.size, orderStatus: that.data.orderStatus}).then(function (res) {
             if (res.errno === 0) {
                 console.log(res.data);
                 if(res.data.totalPages==0){
                     that.setData({
-                        // orderList: that.data.orderList.concat(res.data.data),
-                        // page: res.data.currentPage + 1,
-                        // totalPages: 1,
-                        realTotalPages: res.data.totalPages
-
+                        realTotalPages: res.data.totalPages,
+                        listHeight: wx.getSystemInfoSync().windowHeight -40 ,
+                        listStatus: res.data.data.length < 5 ? 3 : 1,
+                        pageStatus: 0
                     });
+
                 }else{
-                    that.setData({
-                        orderList: that.data.orderList.concat(res.data.data),
-                        page: res.data.currentPage + 1,
-                        totalPages: res.data.totalPages,
-                        realTotalPages: 1
-                    });
-
-                    let c = {
-                        listHeight: wx.getSystemInfoSync().windowHeight - 45,
-                        nomore: res.data.data.length < 5 ? true : false
-                        // pageStatus: 0
-                    };
-                    if(res.data.data.length>0){
-                        that.setData(c)
-                    }
+                        that.setData({
+                            orderList: that.data.orderList.concat(res.data.data),
+                            page: res.data.currentPage + 1,
+                            totalPages: res.data.totalPages,
+                            realTotalPages: 1,
+                            listHeight: wx.getSystemInfoSync().windowHeight -40 ,
+                            listStatus: res.data.data.length < 5 ? 3 : 1,
+                            pageStatus: 0
+                        });
                 }
-
-                that.refresh = false;
-                wx.hideLoading();
+            }else{
+                if(flag){
+                    that.setData({
+                        listStatus: 1,
+                        pageStatus: 2
+                    });
+                }
             }
         });
     },
     payOrder(e){
-       /* wx.redirectTo({
-            url: '/pages/pay/pay',
-        })*/
-        var orderId = e.target.dataset.orderid;
+        let orderId = e.target.dataset.orderid;
         // let that = this;
-
         pay.payOrder(parseInt(orderId)).then(res => {
             wx.redirectTo({
                 url: '/pages/payResult/payResult?status=1&orderId=' + orderId
@@ -110,27 +89,6 @@ Page({
                 url: '/pages/payResult/payResult?status=0&orderId=' + orderId
             });
         });
-        /*util.request(api.PayPrepayId, {
-            orderId: orderId || 15
-        }).then(function (res) {
-            if (res.errno === 0) {
-
-                const payParam = res.data;
-                wx.requestPayment({
-                    'timeStamp': payParam.timeStamp,
-                    'nonceStr': payParam.nonceStr,
-                    'package': payParam.package,
-                    'signType': payParam.signType,
-                    'paySign': payParam.paySign,
-                    'success': function (res) {
-                        console.log(res);
-                    },
-                    'fail': function (res) {
-                        console.log(res);
-                    }
-                });
-            }
-        });*/
 
     },
     updateSuccess() {
@@ -141,25 +99,18 @@ Page({
     gotoOrderByStatus(orderStatus) {
         this.setData({
             page: 1,
+            pageStatus: 1,
             orderList: [],
             size: 5,
             orderStatus: orderStatus
         });
-        wx.showLoading({
-            title: '加载中...',
-            success: function () {
-
-            }
-        });
-        // 加载数据
-        this.getOrderList();
+        this.getOrderList(true);
     },
     gotoAllOrder() {
         this.gotoOrderByStatus("");
     },
     // 待付款
     gotoWaitPayOrder() {
-        debugger
         this.gotoOrderByStatus(0);
     },
     // 待发货
